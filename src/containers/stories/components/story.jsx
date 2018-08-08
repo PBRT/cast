718f8d9fef3b89388406daf71e72f02939e9abb4
mamouel <e-dodier@hotmail.fr>
2018-08-07 22:18:54 -0400

    transformation des stories en cards

--- src/containers/stories/components/story.jsx
+++ src/containers/stories/components/story.jsx
@@ -2,31 +2,65 @@
 import type { Story as StoryType } from "../stories.type.js";
 
 import moment from "moment";
-import Paper from "material-ui/Paper";
 import React, { Component } from "react";
-import Typography from "material-ui/Typography";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-type Props = {
-  story: StoryType
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
 };
 
 
-const Story = (props: Props) => (
-  <div style={{ maxWidth: 800, margin: "auto" }}>
-    <Paper style={{ marginBottom: 12, padding: 12 }}>
-      <Typography type="title">{props.story.title}</Typography>
-      <Typography type="caption">
-        {moment(parseInt(props.story.timestamp)).format("DD-MM-YYYY")}
-      </Typography>
-      <Typography style={{ marginTop: 8 }} type="subheading">
-        {props.story.description}
-      </Typography>
-      <Typography style={{ marginTop: 28 }} type="subheading">
-        {props.story.username}
-      </Typography>
-    </Paper>
-  </div>
-);
 
+function Story (props: Props) {
+  const { classes } = props;
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image="/public/careful-mate.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="headline" component="h2">
+            {props.story.title}
+          </Typography>
+          <Typography type="caption" component="p">
+            {moment(parseInt(props.story.timestamp)).format("DD-MM-YYYY")}
+          </Typography>
+          <Typography style={{ marginTop: 8 }} type="subheading" component="p">
+            {props.story.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
 
-export default Story;
+Story.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Story);
