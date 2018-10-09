2eeedff63d47fe8d79879a460a4a58fd908b9b1f
mamouel <e-dodier@hotmail.fr>
2018-10-09 15:52:25 -0400

    button submit added for search

--- src/containers/stories/stories.container.jsx
+++ src/containers/stories/stories.container.jsx
@@ -11,7 +11,15 @@ import Story from "./components/story";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import LoadingAnimation from "./../../../src/components/loading-animation";
+
+
+library.add(faSearch);
+
 
 type Props = {
   stories: StoriesState,
@@ -45,22 +53,28 @@ class StoriesContainer extends Component<Props> {
     } else {
       return (
         <div>
-          <div style={{ display: "flex", width: "100%" }} >
-            <Button onClick={this.orderStoriesByDesc}>
-              Order by Desc
-            </Button>
-            <Button onClick={this.orderStoriesByAsc}>
-              Order by Asc
-            </Button>
-            <TextField
-              id="outlined-search"
-              label="Search field"
-              type="search"
-              className=""
-              margin="normal"
-              variant="outlined"
-              style={{ position: "relative", float: "right", width: 300 }}
-            />
+          <div style={{ display: "flex", width: "100%", justifyContent: "space-between" }} >
+            <div style={{ margin: 20 }}>
+              <Button onClick={this.orderStoriesByDesc}>
+                Order by Desc
+              </Button>
+              <Button onClick={this.orderStoriesByAsc}>
+                Order by Asc
+              </Button>
+            </div>
+            <div style={{ width: 300 }} >
+              <TextField
+                id="outlined-search"
+                label="Search field"
+                type="search"
+                className=""
+                margin="normal"
+                variant="outlined"
+              />
+              <Button>
+                <FontAwesomeIcon id="search-icon" icon="search" style={{ display: "block"}}/>
+              </Button>
+            </div>
           </div>
           <Section title="Latest stories" info={{ error, timestamp }} style={{ width: "100%" }}>
             <div className="cards-container" style={{ display: "flex", margin: "auto", flexWrap: "wrap" }}>
