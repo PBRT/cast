99ad48d00ade018ca280a933b7738d4fdd723b7d
mamouel <e-dodier@hotmail.fr>
2018-10-09 23:58:48 -0400

    search input with function search (need a fix)

--- src/containers/stories/stories.container.jsx
+++ src/containers/stories/stories.container.jsx
@@ -3,6 +3,7 @@ import type { State } from "../../state/state.type.js";
 import type { Action } from "./stories.actions.js";
 import type { StoriesState, Story as StoryType } from "./stories.type";
 
+import _ from "lodash";
 import { connect } from "react-redux";
 import React, { Component } from "react";
 import { requestStories } from "./stories.actions.js";
@@ -11,9 +12,9 @@ import Story from "./components/story";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import LoadingAnimation from "./../../../src/components/loading-animation";
 
@@ -27,17 +28,35 @@ type Props = {
 };
 
 class StoriesContainer extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm : "",
+      currentDisplay: this.props.stories.stories
+    };
+  };
+
   componentDidMount = () => {
     this.props.dispatch(requestStories("DESC"))
-  }
+  };
+
+  onInputChange = (event) => {
+
+    let newlyDisplayed = _.filter(this.props.stories.stories, story => story.title.includes(event.target.value.toLowerCase()))
+
+    this.setState({
+      searchTerm: event.target.value,
+      currentDisplay: newlyDisplayed
+    });
+  };
 
   orderStoriesByDesc = () => {
     this.props.dispatch(requestStories("DESC"))
-  }
+  };
 
   orderStoriesByAsc = () => {
     this.props.dispatch(requestStories("ASC"))
-  }
+  };
 
   // deleteThisStory = () => {
   //   console.log(this.props)
@@ -46,6 +65,7 @@ class StoriesContainer extends Component<Props> {
 
   render() {
     const { stories: { stories, error, timestamp } } = this.props;
+    console.log(this.state.currentDisplay)
     if(stories.length === 0) {
       return (
         <LoadingAnimation/>
@@ -67,18 +87,19 @@ class StoriesContainer extends Component<Props> {
                 id="outlined-search"
                 label="Search field"
                 type="search"
-                className=""
                 margin="normal"
                 variant="outlined"
+                value={this.state.searchTerm}
+                onChange={this.onInputChange}
               />
-              <Button>
+              <Button onClick={() => this.performSearch(this.state.searchTerm)}>
                 <FontAwesomeIcon id="search-icon" icon="search" style={{ display: "block"}}/>
               </Button>
             </div>
           </div>
           <Section title="Latest stories" info={{ error, timestamp }} style={{ width: "100%" }}>
             <div className="cards-container" style={{ display: "flex", margin: "auto", flexWrap: "wrap" }}>
-              {stories.map((story: StoryType, idx: number ) => (
+              {this.state.currentDisplay.map((story: StoryType, idx: number ) => (
               <Story key={idx} story={story} />
               ))}
             </div>
