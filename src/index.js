810c78c0280a3226b957f7897b8242a36be2f14d
mamouel <e-dodier@hotmail.fr>
2018-11-03 18:38:50 -0400

    only 20 last stories fetched

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import injectTapEventPlugin from "react-tap-event-plugin";
+// import injectTapEventPlugin from "react-tap-event-plugin";
 import Root from "./root";
 import registerServiceWorker from "./utils/registerServiceWorker";
 import { initializeStore } from "./state/store.js";
 import "typeface-roboto";
 import "./index.css";
 
-injectTapEventPlugin();
+// injectTapEventPlugin();
 
 const store = initializeStore();
 
